f2b55271b992923ef688847ad9802e1bd538617a
陳玟穎 <jessicachen@yile.com.tw>
2024-03-14 15:46:28 +0800

    initial setting

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import React from 'react';
+import Banner from './component/Banner/Banner';
+import Info from './component/Info/Info';
+import './App.sass';
+
+// eslint-disable-next-line require-jsdoc
 function App() {
-  return (<div className='main'>Hello World</div>);
+  return (
+    <div className="app">
+      <Banner />
+      <Info />
+    </div>
+  );
 }
 
 export default App;

1e5eceb0aedad3d3d2c31559f51eeaabcf4c316f
陳玟穎 <jessicachen@yile.com.tw>
2024-03-19 11:52:06 +0800

    style: logo & character-eye animation

--- src/component/Banner/Banner.jsx
+++ src/component/Banner/Banner.jsx
@@ -1,9 +1,27 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import style from "../Banner/Banner.module.sass";
 
 const Banner = () => {
+  // 眼睛隨游標移動
+  const [eyeStyle, setEyeStyle] = useState({});
+  const bannerRef = useRef();
+
+  const handleMouseMove = event => {
+    if (bannerRef.current) {
+      const { left, width } = bannerRef.current.getBoundingClientRect();
+      const x = (event.clientX - left) / width;
+
+      const eyeMove = 20;
+      const moveX = (x - 1) * eyeMove;
+
+      setEyeStyle({
+        transform: `translate(${moveX}px)`
+      });
+    }
+  };
+
   return (
-    <div className={style.banner}>
+    <div className={style.banner} ref={bannerRef} onMouseMove={handleMouseMove}>
       <img
         src="/images/Background-01.png"
         width={"100%"}
@@ -13,36 +31,28 @@ const Banner = () => {
       ></img>
       <img
         src="/images/Character-01-White.png"
-        width={"1097px"}
-        height={"823px"}
         alt="banner-character-white"
         className={style.bannerCharacterWhite}
       />
       <img
         src="/images/Character-01.png"
-        width={"1097px"}
-        height={"823px"}
         alt="banner-character"
         className={style.bannerCharacter}
       ></img>
       <img
         src="/images/LeftEye-01.png"
-        width={"39.82px"}
-        height={"32.28px"}
         alt="banner-character-left-eye"
         className={style.bannerCharacterLeftEye}
+        style={eyeStyle}
       ></img>
       <img
         src="/images/RightEye-01.png"
-        width={"39.82px"}
-        height={"32.28px"}
         alt="banner-character-right-eye"
         className={style.bannerCharacterRightEye}
+        style={eyeStyle}
       ></img>
       <img
         src="/images/Logo-01.png"
-        width={"540px"}
-        height={"323px"}
         alt="banner-logo"
         className={style.bannerLogo}
       ></img>

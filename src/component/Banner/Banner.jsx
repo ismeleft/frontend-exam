d3620a6a7266737b734c94c58613945871958de4
陳玟穎 <jessicachen@yile.com.tw>
2024-03-22 11:35:46 +0800

    fix: 修改 Banner 重複渲染問題

--- src/component/Banner/Banner.jsx
+++ src/component/Banner/Banner.jsx
@@ -1,27 +1,44 @@
-import React, { useRef, useState } from "react";
+import React, { useRef, useEffect } from "react";
 import style from "../Banner/Banner.module.sass";
 
 const Banner = () => {
-  // 眼睛隨游標移動
-  const [eyeStyle, setEyeStyle] = useState({});
   const bannerRef = useRef();
+  const leftEyeRef = useRef();
+  const rightEyeRef = useRef();
+  const eyeMove = 10;
 
   const handleMouseMove = event => {
     if (bannerRef.current) {
-      const { left, width } = bannerRef.current.getBoundingClientRect();
+      const { left, top, width, height } =
+        bannerRef.current.getBoundingClientRect();
       const x = (event.clientX - left) / width;
+      const y = (event.clientY - top) / height;
+      const moveX = (x - 0.5) * eyeMove * 2;
+      const moveY = (y - 0.5) * eyeMove * 2;
 
-      const eyeMove = 5;
-      const moveX = (x - 1) * eyeMove;
-
-      setEyeStyle({
-        transform: `translate(${moveX}px)`
-      });
+      if (leftEyeRef.current && rightEyeRef.current) {
+        const transformValue = `translate(${moveX}px,${moveY}px)`;
+        leftEyeRef.current.style.transform = transformValue;
+        rightEyeRef.current.style.transform = transformValue;
+      }
     }
   };
 
+  useEffect(() => {
+    const bannerElement = bannerRef.current;
+    if (bannerElement) {
+      bannerElement.addEventListener("mousemove", handleMouseMove);
+    }
+
+    return () => {
+      if (bannerElement) {
+        bannerElement.removeEventListener("mousemove", handleMouseMove);
+      }
+    };
+  }, []);
+
   return (
-    <div className={style.banner} ref={bannerRef} onMouseMove={handleMouseMove}>
+    <div className={style.banner} ref={bannerRef}>
       <img
         src="/images/Background-01.png"
         width={"100%"}
@@ -44,13 +61,13 @@ const Banner = () => {
           src="/images/LeftEye-01.png"
           alt="banner-character-left-eye"
           className={style.bannerCharacterLeftEye}
-          style={eyeStyle}
+          ref={leftEyeRef}
         />
         <img
           src="/images/RightEye-01.png"
           alt="banner-character-right-eye"
           className={style.bannerCharacterRightEye}
-          style={eyeStyle}
+          ref={rightEyeRef}
         />
       </div>
 
